f5ef4168d819b84d3a0e984c3e6bb67e0a1022ab
kudinov <s.kudinov@list.ru>
2024-07-02 14:24:31 +0300

    6.0 perfomance opt

--- image_app.py
+++ image_app.py
@@ -5,15 +5,16 @@ import requests
 from io import BytesIO
 
 
-def svd(img, top_k):
-    U, sing_values, V = np.linalg.svd(img)
-    sigma = np.zeros(shape=img.shape)
-    np.fill_diagonal(sigma, sing_values)
-    trunc_U = U[:, :top_k]
-    trunc_sigma = sigma[:top_k, :top_k]
-    trunc_V = V[:top_k, :]
-    new_image = trunc_U @ trunc_sigma @ trunc_V
-    return new_image
+def svd_compression(img, top_k):
+    U, s, Vt = np.linalg.svd(img, full_matrices=False)
+    compressed_img = (U[:, :top_k] @ np.diag(s[:top_k]) @ Vt[:top_k, :])
+    return compressed_img
+
+
+def load_image(url):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    return img
 
 
 st.title('Сингулярный преобразователь')
@@ -27,47 +28,34 @@ if 'image' not in st.session_state:
     st.session_state.image = None
 
 if button:
-    response = requests.get('https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png')
-    url_image = Image.open(BytesIO(response.content))
-    st.session_state.image = url_image
+    st.session_state.image = load_image('https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png')
 
 if uploaded_image:
-    image = Image.open(uploaded_image)
-    st.session_state.image = image
+    st.session_state.image = Image.open(uploaded_image)
 
 if st.session_state.image is not None:
     image_raw = st.session_state.image
+    image_array = np.array(image_raw)
+    image_gray = np.array(image_raw.convert('L'))
 
-    image_show = np.array(image_raw)
-    image = np.array(image_raw.convert('L'))
+    st.caption(f'Разрешение изображения: {image_array.shape[0]}x{image_array.shape[1]}')
+    st.image(image_array, caption='Оригинал', use_column_width=True)
 
-    st.caption(f'Разрешение изображения: {image_show.shape[0]}x{image_show.shape[1]}')
-    st.image(image_show, caption='Оригинал', use_column_width=True)
+    with st.sidebar.form(key='compression_form'):
+        top_k = st.slider('Выберите top K для сжатия', min_value=1, max_value=min(image_gray.shape))
+        color_choice = st.toggle('Цветное')
+        submit_button = st.form_submit_button(label="Сжать")
 
-    with st.sidebar.form(key='my_form'):
-        top_k = st.slider('Выберите top K для сжатия', min_value=1, max_value=min(image.shape))
-        choice = st.toggle('Цветное')
-        submit = st.form_submit_button(label="Submit")
-
-    if submit:
+    if submit_button:
         st.caption(f'Вы выбрали top K: {top_k}')
-
-        if top_k:
-            with st.spinner(text="In progress..."):
-                if not choice:
-                    new_image = svd(image, top_k)
-
-                else:
-                    img_r = np.array(image_raw)[:, :, 0]
-                    img_g = np.array(image_raw)[:, :, 1]
-                    img_b = np.array(image_raw)[:, :, 2]
-
-                    new_image_r = svd(img_r, top_k)
-                    new_image_g = svd(img_g, top_k)
-                    new_image_b = svd(img_b, top_k)
-
-                    new_image = np.stack((new_image_r, new_image_g, new_image_b), axis=-1)
-
-                new_image = (new_image - np.min(new_image)) / (np.max(new_image) - np.min(new_image))
-
-            st.image(new_image, caption='Сжатое изображение', use_column_width=True)
+        with st.spinner(text="Сжатие..."):
+            if not color_choice:
+                compressed_image = svd_compression(image_gray, top_k)
+                compressed_image = (compressed_image - np.min(compressed_image)) / (np.max(compressed_image) - np.min(compressed_image))
+                st.image(compressed_image, caption='Сжатое изображение (ЧБ)', use_column_width=True)
+            else:
+                channels = [image_array[:, :, i] for i in range(3)]
+                compressed_channels = [svd_compression(channel, top_k) for channel in channels]
+                compressed_image = np.stack(compressed_channels, axis=-1)
+                compressed_image = (compressed_image - np.min(compressed_image)) / (np.max(compressed_image) - np.min(compressed_image))
+                st.image(compressed_image, caption='Сжатое изображение (Цветное)', use_column_width=True)
